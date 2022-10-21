854595ebb727fc716072673c38fd9492fcc0fcf0
Sanyog Yadav <sanyogyadav52@gmail.com>
2022-10-21 14:58:35 +0530

    All the changes has been done..

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', () => {
     const title = localStorage.getItem('TITLE');
     const article = localStorage.getItem('ARTICLE');
+    const title1 = sessionStorage.getItem('TITLE1');
+    const article1 = sessionStorage.getItem('ARTICLE1');
 
     document.getElementById('b_title').innerHTML = title;
     document.getElementById('b_article').innerHTML = article;
+    document.getElementById('b_title').innerHTML = title1;
+    document.getElementById('b_article').innerHTML = article1;
 })
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,10 @@
 function handleSubmit() {
     const title = document.getElementById('blog_title').value;
     const article = document.getElementById('blog_article').value;
-    const file = document.getElementById('file-upload');
 
     localStorage.setItem("TITLE",title);
     localStorage.setItem("ARTICLE",article);
-
-    file.addEventListener('change', () =>{
-        const fr = new FileReader();
-
-        fr.readAsDataURL(file.files[0]);
-        fr.addEventListener('load', () => {
-            const url = fr.result;
-            console.log(url);
-        })
-    })
+    sessionStorage.setItem("TITLE1",title);
+    sessionStorage.setItem("ARTICLE1",article);
     return;
 }
